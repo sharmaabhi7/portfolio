7dfd86f87abff521dccd2b04005d810ae2a38bfc
sharmaabhi7 <abhis70411@gmail.com>
2024-05-09 09:43:58 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text",{
-    strings:["Freelancer","Front-End Developer","Photo Editor","Video Editor","3D Modeling"],
-    typeSpeed:50,
-    backSpeed:50,
+    strings:["Freelancer","Front-End Developer","Youtuber","3D Modeling"],
+    typeSpeed:100,
+    backSpeed:100,
     backDelay:500,
     loop:true
 });
